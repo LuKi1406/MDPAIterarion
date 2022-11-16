c9d5ad077ced8a16ae086e9acfa0505dd20912f3
Josipa Gligić <32097034+LuKi1406@users.noreply.github.com>
2022-11-16 18:43:35 +0100

    Add files via upload

---
+++ JUnitTest.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package mdpPackage.threadsPunish;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Gligic
+ *
+ */
+class JUnitTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
